7ef9a2c751679cb7625135c0a51576f0bccf4f15
chrispaganon <info@chrispaganon.com>
2022-12-28 14:11:55 -0500

    Remove a project reference

--- wp-content/themes/my-child-theme/functions.php
+++ wp-content/themes/my-child-theme/functions.php
@@ -4,9 +4,9 @@ if (!defined('ABSPATH')) exit;
 /**
  * Enqueue styles
  */
-add_action('wp_enqueue_scripts', 'bati_enqueue_styles', 10);
+add_action('wp_enqueue_scripts', 'my_child_theme_enqueue_styles', 10);
 
-function bati_enqueue_styles() {
+function my_child_theme_enqueue_styles() {
   wp_enqueue_style('my-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('parent-theme-style'), rand(111, 9999));
   wp_enqueue_style('my-child-theme-general-css', get_stylesheet_directory_uri() . '/assets/css/my-child-theme-general.css', array('my-child-theme-css'), rand(111, 9999));
 }
